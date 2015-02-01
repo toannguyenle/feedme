b87a670ee0ee88318a5f8064db4ff26eb3009526
toannguyenle <toan@me.com>
2015-01-31 20:55:33 -0800

    update list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,12 +6,13 @@ class HomeController < ApplicationController
     # doc = Nokogiri::HTML(open('http://catalog.lapl.org/carlweb/jsp/DoSearch?databaseID=965&initialsearch=true&count=10&finish=search_page.jsp&mode=manual&terms=ruby+on+rails&Search=Search&index=w'))
     doc = Nokogiri::HTML(open('http://www.promotioncode.org/socialshare.php?cid=1976700&xid=85318'))
     uber = Nokogiri::HTML(open('http://www.promotioncode.org/Uber/1976700/'))
-    sprig = Nokogiri::HTML(open('http://www.promotioncode.org/socialshare.php?cid=1974668&xid=35889'))
+    # sprig = Nokogiri::HTML(open('http://www.promotioncode.org/socialshare.php?cid=1974668&xid=35889'))
+    toangular = Nokogiri::HTML(open('http://www.toangular.com'))
 
-    doc.css('.titleListTitle').each do |link|
-      puts link.content
-    end
-    render html: doc
+    # doc.css('.titleListTitle').each do |link|
+    #   puts link.content
+    # end
+    render 'works'
   end
 end
 
